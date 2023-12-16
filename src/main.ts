47943b11020fc3f9a43d134ccf6b4f65565c5598
HorrorOTB <nicatabdullazade135@gmail.com>
2023-12-16 22:01:29 +0400

    add conf

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3000;
+
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT);
+
+  const logger = new Logger();
+  logger.log(`Application running on port ${PORT}`);
 }
 bootstrap();
